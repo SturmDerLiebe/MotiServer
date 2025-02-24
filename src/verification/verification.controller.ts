8b8e6b9dce959b7c21d42ed2ed68b25d5f03c527
Çağatay <114055447+cagataycode@users.noreply.github.com>
2025-02-24 11:40:28 +0100

    Update src/verification/verification.controller.ts
    Co-authored-by: Christian Sturm <79197480+SturmDerLiebe@users.noreply.github.com>

--- src/verification/verification.controller.ts
+++ src/verification/verification.controller.ts
@@ -18,6 +18,7 @@ export class VerificationController {
     @Post('confirm')
     confirmVerification(@Body('token') token: string) {
         try {
+            // TODO: #17 - Please use Exception Filters to treat Invalidity and Errors
             const isValid =
                 this.verificationService.validateVerificationToken(token);
             if (isValid) {
@@ -27,7 +28,6 @@ export class VerificationController {
             }
         } catch (error) {
             return {
-                // is this ok?
                 message: 'error confirming verification',
                 error: error instanceof Error ? error.message : 'unknown error',
             };
