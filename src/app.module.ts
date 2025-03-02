ee516d0f9e81ef702f9528215db2e48896f70275
Christian Sturm <christian.sturm@code.berlin>
2025-03-02 23:50:18 +0100

    fix: scope AuthService and LocalStrategy to AuthModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,11 @@ import { AppService } from './app.service';
 import { VerificationModule } from './verification/verification.module';
 import { AuthModule } from './auth/auth.module';
 import { PassportModule } from '@nestjs/passport';
-import { AuthService } from './auth/auth.service';
-import { LocalStrategy } from './auth/local.strategy';
 import { SecurityModule } from './security/security.module';
 
 @Module({
     imports: [PassportModule, VerificationModule, AuthModule, SecurityModule],
     controllers: [AppController],
-    providers: [AppService, AuthService, LocalStrategy],
+    providers: [AppService],
 })
 export class AppModule {}
