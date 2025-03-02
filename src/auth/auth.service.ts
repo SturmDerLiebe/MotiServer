f6ef4d66a62c7e09339ce18ba06938a2c0d7699a
Christian Sturm <christian.sturm@code.berlin>
2025-03-02 13:40:55 +0100

    feat: add auth module, service and todos

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    // TODO: async validateUser(username: string, pass: string): Promise<boolean> {
+    validateUser(username: string, pass: string): boolean {
+        const user = { username: 'TestUser', encPassword: 'TestPw' }; // TODO: get user by username form user.service #26
+        return user !== null && pass === user.encPassword; // TODO: use encrypted comparison #31
+    }
+}
