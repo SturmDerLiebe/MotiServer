196ec45ab12b8728c970fceb53c0b522a1f18558
Christian Sturm <christian.sturm@code.berlin>
2025-03-02 14:32:18 +0100

    feat: add temporary login endpoint
    squash in app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    getHello(): string {
+        return this.appService.getHello();
+    }
+
+    @UseGuards(AuthGuard('local'))
+    @Post('auth/login')
+    login() {
+        // TODO: Full Endpoint is done in #16
+        return 'Successfully logged in!';
+    }
 }
