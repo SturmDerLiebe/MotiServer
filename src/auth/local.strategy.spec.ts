94313699dfd67dc30d638195b94be7bebb6a5140
Christian Sturm <christian.sturm@code.berlin>
2025-03-02 16:21:28 +0100

    chore: add local strategy test and mock

---
+++ src/auth/local.strategy.spec.ts
@@ -0,0 +1,35 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.strategy';
+import { AuthServiceMock } from './__mock__/auth.service';
+import { UnauthorizedException } from '@nestjs/common';
+
+describe('LocalStrategy', () => {
+    let localStrategy: LocalStrategy;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                LocalStrategy,
+                { provide: AuthService, useValue: AuthServiceMock },
+            ],
+        }).compile();
+
+        localStrategy = module.get(LocalStrategy);
+    });
+
+    it('should be defined', () => {
+        expect(localStrategy).toBeDefined();
+    });
+
+    describe('validate', () => {
+        it('should throw UnauthorizedException on invalid result from AuthService', () => {
+            //WHEN
+            const testFn = () =>
+                localStrategy.validate('WrongUsername', 'WrongPassword');
+
+            //THEN
+            expect(testFn).toThrow(UnauthorizedException);
+        });
+    });
+});
