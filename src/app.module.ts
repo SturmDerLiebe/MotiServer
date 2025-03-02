e2f2c2cd153233fe6734a255c96130cc109c8236
Christian Sturm <christian.sturm@code.berlin>
2025-03-02 13:42:24 +0100

    chore: link auth service and local strategy in root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VerificationModule } from './verification/verification.module';
 import { AuthModule } from './auth/auth.module';
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from './auth/auth.service';
+import { LocalStrategy } from './auth/local.strategy';
 
 @Module({
-    imports: [VerificationModule, AuthModule],
+    imports: [PassportModule, VerificationModule, AuthModule],
     controllers: [AppController],
-    providers: [AppService],
+    providers: [AppService, AuthService, LocalStrategy],
 })
 export class AppModule {}
