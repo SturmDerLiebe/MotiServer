17e3644dc0f3f0688d662e06d63cb10823d34e28
Christian Sturm <christian.sturm@code.berlin>
2025-03-03 16:41:58 +0100

    feat: enable global ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,19 @@ import { VerificationModule } from './verification/verification.module';
 import { AuthModule } from './auth/auth.module';
 import { PassportModule } from '@nestjs/passport';
 import { SecurityModule } from './security/security.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [PassportModule, VerificationModule, AuthModule, SecurityModule],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: ['.env', '.env.development'],
+        }),
+        PassportModule,
+        VerificationModule,
+        AuthModule,
+        SecurityModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
