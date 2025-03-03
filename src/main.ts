6fa870b2399581c3854794407396e8d481791b94
Christian Sturm <christian.sturm@code.berlin>
2025-03-03 16:42:28 +0100

    feat: implement express session

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import * as process from 'node:process';
+import Redis from 'ioredis';
+import { RedisStore } from 'connect-redis';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.use(
+        session({
+            secret: process.env.SESSION_SECRET as string,
+            resave: false,
+            saveUninitialized: false,
+            store:
+                (process.env.NODE_ENV as string) !== 'development'
+                    ? undefined
+                    : new RedisStore({
+                          client: new Redis(
+                              process.env.UPSTASH_SESSION_REDIS_URL as string,
+                          ),
+                      }),
+        }),
+    );
+
     await app.listen(process.env.PORT ?? 3000);
 }
 
