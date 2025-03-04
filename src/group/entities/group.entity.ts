da39c596112fce5205b0844be17873df2ba9dbc8
Cagatay Uslu <cagatay.uslu@code.berlin>
2025-03-04 13:00:05 +0100

    add group module

---
+++ src/group/entities/group.entity.ts
@@ -0,0 +1,31 @@
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('groups')
+export class Group {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    groupName: string;
+
+    @Column('simple-array', { nullable: true })
+    memberIds: string[];
+
+    // @Column({ nullable: true })
+    // createdById: string;
+
+    @Column({ nullable: true })
+    inviteCode: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
