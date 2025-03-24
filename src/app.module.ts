ff34e15e88e6ea0083b7397b6d09da196c4f80ed
fbachus <felix.bachstein@gmail.com>
2025-03-24 18:22:53 +0100

    feat(users): add outline to get started on user handling

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { EmailModule } from './email/email.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as process from 'node:process';
 import { RedisOptions } from 'ioredis';
+import { UsersModule } from './users/users.module';
 
 @Module({
     imports: [
@@ -49,6 +50,7 @@ import { RedisOptions } from 'ioredis';
         AuthModule,
         ImageModule,
         EmailModule,
+        UsersModule,
     ],
     controllers: [AppController],
     providers: [AppService],
