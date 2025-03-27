d7f1fe1f1d82adb8a9a34f86deb7b631e0bfe9c5
Christian Sturm <christian.sturm@code.berlin>
2025-03-27 16:38:18 +0100

    chore: format

--- src/security/security.spec.ts
+++ src/security/security.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { SecurityProvider } from './security.provider';
 
 describe('Security', () => {
-  let provider: SecurityProvider;
+    let provider: SecurityProvider;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [SecurityProvider],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [SecurityProvider],
+        }).compile();
 
-    provider = module.get<SecurityProvider>(SecurityProvider);
-  });
+        provider = module.get<SecurityProvider>(SecurityProvider);
+    });
 
-  it('should be defined', () => {
-    expect(provider).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(provider).toBeDefined();
+    });
 });
