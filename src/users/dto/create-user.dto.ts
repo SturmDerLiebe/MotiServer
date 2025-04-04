e486fd25eb9c9761717f4bc5adee79ccea804501
fbachus <felix.bachstein@gmail.com>
2025-04-04 19:31:46 +0200

    feat(users): finish frame for user module

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,7 @@
+export class CreateUserDto {
+    id: number;
+    name: string;
+    email: string;
+    account_status: boolean;
+    password: string;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,16 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Post,
+    Put,
+    Delete,
+    Req,
+    Param,
+    Body,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
@@ -7,18 +19,22 @@ export class UsersController {
         return 'This action creates a user'; //TODO: create user
     }
     @Get()
+    // TODO: implement authorization and admin guard
     findAll(): string {
         return 'Trying to get all users?';
     }
     @Get(':id')
+    // TODO: implement authorization and user guard
     findOne(@Param('id') id: string): string {
         return `Return the json for user ${id}`; //TODO: get user data
     }
     @Put(':id')
+    // TODO: implement authorization and user guard
     update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
         return `This action updates user ${id}`; //TODO: update user
     }
     @Delete(':id')
+    // TODO: implement authorization and user guard
     remove(@Param('id') id: string) {
         return `This action removes user ${id}`; //TODO: delete user
     }
