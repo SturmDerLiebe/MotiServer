51e166c85e10275ae2da9bccf4390747d6c6f882
Cagatay Uslu <cagatay.uslu@code.berlin>
2025-04-07 16:18:06 +0200

    chore: update User entity with relationships with webauthn

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -1,13 +1,25 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { PasskeyEntity } from './passkey.entity';
+import { Challenge } from './challenge.entity';
 
 @Entity()
 export class User {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column()
+    @Column({ unique: true })
     email: string;
 
     @Column()
     password: string;
+
+    @OneToMany(() => PasskeyEntity, (passkey) => passkey.user, {
+        cascade: true,
+    })
+    passkeys: PasskeyEntity[];
+
+    @OneToMany(() => Challenge, (challenge) => challenge.user, {
+        cascade: true,
+    })
+    challenges: Challenge[];
 }
