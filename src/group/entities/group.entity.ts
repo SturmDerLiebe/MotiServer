3914ad00a0b5a0a71fef88faf840395ee85afef0
Cagatay Uslu <cagatay.uslu@code.berlin>
2025-04-11 14:03:54 +0200

    chore: add temporary memberIds and createdById columns to group.entity

--- src/group/entities/group.entity.ts
+++ src/group/entities/group.entity.ts
@@ -14,11 +14,13 @@ export class Group {
     @Column()
     groupName: string;
 
+    // Temporary column for member IDs (to be replaced by ManyToMany relationship)
     @Column('simple-array', { nullable: true })
-    memberIds: string[];
+    memberIds: string[]; // TODO: Replace with ManyToMany relationship with User entity
 
+    // Temporary column for creator ID (to be replaced by ManyToOne relationship)
     @Column({ nullable: true })
-    createdById: string;
+    createdById: string; // TODO: Replace with ManyToOne relationship with User entity
 
     @Column({ nullable: true })
     inviteCode: string;
