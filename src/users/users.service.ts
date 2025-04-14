12f75f2210a7d2f899a6dcc4ed024c5e0b929152
fbachus <felix.bachstein@gmail.com>
2025-04-14 17:27:37 +0200

    add(user): add user module

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,25 +4,38 @@ import { User } from './interfaces/user.interface';
 @Injectable()
 export class UsersService {
     private users: Map<number, User> = new Map();
+    private userIDcounter: number = 0;
 
-    create(user: User) {
+    create(user: User): number {
         // TODO: #26 - add user to database if it does not exist yet
-        this.users.set(user.id, user);
+        this.userIDcounter += 1;
+        this.users.set(this.userIDcounter, user);
+        return this.userIDcounter;
     }
     findAll() {
         // TODO: #26 - get all users from DB
-        return this.users;
+        return Array.from(this.users.values());
     }
-    findOne(userID: number): any { //returns User or undefined
-        // TODO: #26 - get user info from DB
-        return this.users.get(userID);
+    findOne(userID: number): User | undefined {
+        //returns User or undefined
+        // TODO: #26 - get user info from
+
+        // the + ensures that the type is number, which on testing with postman
+        // was not the case despite the type being number in the function def
+        // parseInt will not work because the defined type is already "number"
+        return this.users.get(+userID);
     }
     update(userID: number, user: User): boolean {
         // TODO: #26 - update user in DB
-        return true;
+        if (this.users.has(+userID)) {
+            this.users.set(+userID, user);
+            return true;
+        } else {
+            return this.users.has(+userID);
+        }
     }
     remove(userID: number): boolean {
         // TODO: #26 - delete user from DB
-        return this.users.delete(userID);
+        return this.users.delete(+userID);
     }
 }
