f87f2acb941a19ecb7dcf8ce35f614be34a6c1ce
fbachus <felix.bachstein@gmail.com>
2025-04-16 15:56:56 +0200

    fix(project): fix import of supabase.service.ts to correct server start
    fix #89

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EmailService } from './email.service';
+import { BucketModule } from 'src/bucket/bucketModule';
 
 @Module({
+    imports: [BucketModule],
     providers: [EmailService],
 })
 export class EmailModule {}
