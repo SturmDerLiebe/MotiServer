252ef50aaadca8b5516ad3c3424c6b41cc1030fd
Christian Sturm <christian.sturm@code.berlin>
2025-04-17 09:57:52 +0200

    chore: rename file according to NestJS conventions

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { EmailService } from './email.service';
-import { BucketModule } from 'src/bucket/bucketModule';
+import { BucketModule } from 'src/bucket/bucket.module';
 
 @Module({
     imports: [BucketModule],
