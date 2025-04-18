813513dbf829b3e8535b61a67f5b68c376a1ddac
Cagatay Uslu <cagatay.uslu@code.berlin>
2025-04-18 14:19:30 +0200

    add passkey implementation

---
+++ src/auth/entities/challenge.entity.ts
@@ -0,0 +1,26 @@
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    ManyToOne,
+    CreateDateColumn,
+} from 'typeorm';
+import { User } from './user.entity';
+
+@Entity()
+export class Challenge {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @ManyToOne(() => User, (user) => user.challenges, { onDelete: 'CASCADE' })
+    user: User;
+
+    @Column()
+    challenge: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @Column({ type: 'timestamp', nullable: true })
+    expiresAt: Date; // Optional expiration logic
+}
