d1b90bd312adc512074668eef9127a8c68c6ccd9
Till1983 <till_ermold@web.de>
2025-04-22 21:37:28 +0200

    Add message entity

---
+++ src/message/entities/message.entity.ts
@@ -0,0 +1,45 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    ManyToOne,
+    OneToMany,
+    JoinColumn,
+    CreateDateColumn,
+} from 'typeorm';
+import { User } from '../user/user.entity';
+import { Group } from '../group/group.entity';
+import { MessageReaction } from '../message-reaction/message-reaction.entity';
+
+
+@Entity()
+export class Message {
+    @PrimaryGeneratedColumn()
+    message_id: number;
+
+    @Column()
+    sender_id: number;
+
+    @Column()
+    group_id: number;
+
+    @Column({ length: 50 })
+    message_type: string;
+
+    @Column({ length: 2000 })
+    content: string;
+
+    @CreateDateColumn()
+    sent_at: Date;
+
+    @ManyToOne(() => User, (user) => user.messages)
+    @JoinColumn({ name: "sender_id" })
+    sender: User;
+
+    @ManyToOne(() => Group, (group) => group.messages)
+    @JoinColumn({ name: "group_id" })
+    group: Group;
+
+    @OneToMany(() => MessageReaction, (reaction) => reaction.message)
+    reactions: MessageReaction[];
+}
