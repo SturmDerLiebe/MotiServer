b3ee9ee7b33b466ccea7561b2a78403a56e45ea8
Till1983 <till_ermold@web.de>
2025-04-23 11:43:40 +0200

    Add group challenge entity

---
+++ src/group-member/entities/group-member.entity.ts
@@ -0,0 +1,33 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    ManyToOne,
+    JoinColumn,
+    CreateDateColumn,
+} from 'typeorm';
+import { User } from '../../user//entities/user.entity';
+import { Group } from '../../group/entities/group.entity';
+
+@Entity()
+export class GroupMember {
+    @PrimaryGeneratedColumn()
+    member_id: number;
+
+    @Column()
+    group_id: number;
+
+    @Column()
+    user_id: number;
+
+    @CreateDateColumn()
+    joined_at: Date;
+
+    @ManyToOne(() => User, (user) => user.groupMemberships)
+    @JoinColumn({ name: 'user_id' })
+    user: User;
+
+    @ManyToOne(() => Group, (group) => group.members)
+    @JoinColumn({ name: 'group_id' })
+    group: Group;
+}
