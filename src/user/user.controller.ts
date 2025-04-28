113ad1882adc313f4786a317deb9e1abc949cd69
fbachus <felix.bachstein@gmail.com>
2025-04-28 17:49:51 +0200

    feat(user): build out controller to serve all data

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,6 +5,7 @@ import {
     Put,
     Delete,
     Param,
+    Req,
     Body,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -25,10 +26,55 @@ export class UserController {
     findAll() {
         return this.userService.findAll();
     }
-    @Get(':id')
+    @Get()
     // TODO: implement authorization and user guard
-    findOne(@Param('id') id: number): any {
-        return this.userService.findOne(id) || 'user not found';
+    // TODO: return all relevant data for one user
+    // user:
+    // - name
+    // - email
+    // - NOT password
+    // - profile picture
+    // groups:
+    // - group names?
+    // challenge:
+    // - target count
+    // - achieved count
+    // - current progress
+    // - created at
+    // group challenge:
+    // - description
+    // - start date
+    // - end date
+    // - created at
+    // - updated at
+    findOne(@Req() id: number): any {
+        const user = this.userService.findOne(id);
+        if (!user) {
+            return 'user not found';
+        }
+        return {
+            // TODO: return data on a users groups
+            user: {
+                name: user.name,
+                email: user.email,
+                groups: 'This would be a list of the group names if we support\
+                many, or the name of the single group one user is part of',
+            },
+            //TODO: return actual data to user challenge
+            user_challenge: {
+                target_count: 'return target count',
+                achieved_count: 'return achieved count',
+                current_progress: 'return current progress',
+            },
+            //TODO: return data about group challenge
+            group_challenge: {
+                description: 'return description',
+                start_date: 'return start_date',
+                end_date: 'return end_date',
+                created_at: 'return created_at',
+                updated_at: 'return updated_at',
+            },
+        };
     }
     @Put(':id')
     // TODO: implement authorization and user guard
