db734e6ae8ecc025b51c9d30bf92fc9fd0e8606e
Till1983 <till_ermold@web.de>
2025-05-12 19:50:36 +0200

    Add ORM entities

---
+++ src/google-oauth/entities/authentication.entity.ts
@@ -0,0 +1,27 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    OneToOne,
+    JoinColumn,
+} from 'typeorm';
+import { User } from '../../user/entities/user.entity';
+
+@Entity()
+export class Authentication {
+    @PrimaryGeneratedColumn()
+    auth_id: number;
+
+    @Column()
+    user_id: number;
+
+    @Column({ length: 100, nullable: true })
+    google_id: string;
+
+    @Column({ length: 50 })
+    source: string;
+
+    @OneToOne(() => User, (user) => user.authentication)
+    @JoinColumn({ name: 'user_id' })
+    user: User;
+}
