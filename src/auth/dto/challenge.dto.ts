a28c7c963c12b844dfa23e63db4e45ec30ab7c4e
Till1983 <till_ermold@web.de>
2025-06-12 11:33:24 +0200

    Add API documentation for ChallengeDTO

--- src/auth/dto/challenge.dto.ts
+++ src/auth/dto/challenge.dto.ts
@@ -1,7 +1,21 @@
 import { Challenge } from '../entities/challenge.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ChallengeDTO {
+    @ApiProperty({
+        description: 'The challenge string for WebAuthn registration',
+        example: 'dGhpcyBpcyBhIHNhbXBsZSBjaGFsbGVuZ2U=',
+        type: String,
+        required: true,
+    })
     challenge: string;
+
+    @ApiProperty({
+        description: 'The expiration date of the challenge',
+        example: '2024-10-01T12:00:00Z',
+        type: Date,
+        required: true,
+    })
     expiresAt: Date;
 
     constructor(challenge: Challenge) {
