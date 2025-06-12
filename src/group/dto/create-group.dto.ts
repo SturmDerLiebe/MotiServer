aa08ee03e42ed47e09e08694359e191f25bd2422
Till1983 <till_ermold@web.de>
2025-06-12 10:47:48 +0200

    Add API documentation for CreateGroupDto

--- src/group/dto/create-group.dto.ts
+++ src/group/dto/create-group.dto.ts
@@ -1,10 +1,22 @@
 import { IsNotEmpty, IsString, IsOptional, IsArray } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateGroupDto {
+    @ApiProperty({
+        description: 'The name of the group',
+        example: 'Fitness Enthusiasts',
+        maxLength: 100,
+        type: String,
+    })
     @IsNotEmpty()
     @IsString()
     groupName: string;
 
+    @ApiProperty({
+        description: 'The ids of the members to be added to the group',
+        example: ['user123', 'user456'],
+        type: [String],
+    })
     @IsOptional()
     @IsArray()
     @IsString({ each: true })
